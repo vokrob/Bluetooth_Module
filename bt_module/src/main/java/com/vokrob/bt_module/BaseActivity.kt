08b6d6635b526882c468dadc91da33a9d7ab8970
vokrob <106639488+vokrob@users.noreply.github.com>
2024-11-05 20:18:17 +0700

    Creating a bluetooth library module and adding permissions

---
+++ bt_module/src/main/java/com/vokrob/bt_module/BaseActivity.kt
@@ -0,0 +1,20 @@
+package com.vokrob.bt_module
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class BaseActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_base)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.placeHolder)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
